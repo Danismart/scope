ff761cb61237d15633df10743701cda2376c4ab8
Daniela Alonso Alonso <desarrollador7@smartquick.com.co>
2019-11-26 15:05:57 -0500

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,30 @@
+console.log("Hola mundo");
+var trabajo = "pesado";
+console.log("pesado");
+if (trabajo) {
+  var trabajo = "fácil";
+  console.log("dentro del if", trabajo);
+}
+console.log("fuera del if", trabajo);
+
+//ejercisiouno
+
+let nombre = "Daniela";
+if (nombre) {
+  let nombre = "alejo";
+  let nombre = "liliana";
+  console.log("nombre", nombre);
+  console.log("dentro del if", nombre);
+}
+
+console.log("fuera del if", nombre);
+
+//ejercisiodos
+
+const animale = "léon";
+if (animale) {
+  const animale = "gato";
+  console.log(animale);
+  console.log(trabajo, nombre);
+}
+console.log(animale);
